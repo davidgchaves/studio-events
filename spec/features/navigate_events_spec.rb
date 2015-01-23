2d4fe6924985d93d272d62ad8373df07c60e7a9c
David G Chaves <davidgchaves@gmail.com>
2015-01-23 11:34:18 +0100

    Fix last spec formatting to better conform to AAA

--- spec/features/navigate_events_spec.rb
+++ spec/features/navigate_events_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 describe "Navigating events" do
+
   it "allows navigation from the detail page to the listing page" do
     event = Event.create name: "BugSmash",
                          location: "Denver",
                          price: 10.00,
                          description: "A fun evening of bug smashing",
                          starts_at: 10.days.from_now
-
     visit event_url(event)
 
     click_link "All Events"
