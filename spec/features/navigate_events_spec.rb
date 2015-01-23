3a3bbffcec4e8801e6c48598f46f4bb355d8a14b
David G Chaves <davidgchaves@gmail.com>
2015-01-23 11:31:48 +0100

    RED: Navigating events from the detail page to the listing page

---
+++ spec/features/navigate_events_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "Navigating events" do
+  it "allows navigation from the detail page to the listing page" do
+    event = Event.create name: "BugSmash",
+                         location: "Denver",
+                         price: 10.00,
+                         description: "A fun evening of bug smashing",
+                         starts_at: 10.days.from_now
+
+    visit event_url(event)
+
+    click_link "All Events"
+
+    expect(current_path).to eq events_path
+  end
+end
