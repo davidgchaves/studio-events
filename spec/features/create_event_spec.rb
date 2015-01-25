60b936082b00cdaa398d7501ae90212418c3af97
David G Chaves <davidgchaves@gmail.com>
2015-01-25 12:23:58 +0100

    RED: Creating a new event shows the new event's details

--- spec/features/create_event_spec.rb
+++ spec/features/create_event_spec.rb
@@ -10,4 +10,18 @@ describe "Creating a new event" do
     expect(find_field("Location").value).to eq nil
     expect(find_field("Price").value).to eq nil
   end
+
+  it "shows the new event's details" do
+    visit events_url
+    click_link "Add New Event"
+    fill_in "Name", with: "Code and Coffee"
+    fill_in "Description", with: "Sling some code with a cup o' Joe!"
+    fill_in "Location", with: "Portland, OR"
+    fill_in "Price", with: 0.00
+    #select (10.days.from_now).to_s, from: "event_starts_at_1i"
+
+    click_button "Create Event"
+
+    expect(page).to have_text "Code and Coffee"
+  end
 end
