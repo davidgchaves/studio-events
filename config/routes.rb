c7c00d4d6b13fb87a4dcf33c3b3242e279d3d032
David G Chaves <davidgchaves@gmail.com>
2015-01-27 08:46:18 +0100

    Clean up routes a bit using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   root "events#index"
-  get "events" => "events#index"
-  post "events" => "events#create"
-  get "events/new" => "events#new", as: "new_event"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
-  delete "events/:id" => "events#destroy"
+  resources :events
 end
