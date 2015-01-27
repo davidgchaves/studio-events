f46f72380bc79c8bbff972b5ed8b80bd4a0dfe38
David G Chaves <davidgchaves@gmail.com>
2015-01-27 09:48:37 +0100

    Clean up "Creating a new event" scenario

--- spec/features/create_event_spec.rb
+++ spec/features/create_event_spec.rb
@@ -1,27 +1,38 @@
 require "rails_helper"
 
 describe "Creating a new event" do
-  it "shows the form to create events" do
+  before do
     visit events_url
-
     click_link "Add New Event"
+  end
 
+  it "goes to the create page" do
+    expect(current_path).to eq new_event_path
+  end
+
+  it "shows the form to create events" do
     expect(find_field("Name").value).to eq nil
     expect(find_field("Location").value).to eq nil
     expect(find_field("Price").value).to eq nil
   end
 
-  it "shows the new event's details" do
-    visit events_url
-    click_link "Add New Event"
-    fill_in "Name", with: "Code and Coffee"
-    fill_in "Description", with: "Sling some code with a cup o' Joe!"
-    fill_in "Location", with: "Portland, OR"
-    fill_in "Price", with: 0.00
-    #select (10.days.from_now).to_s, from: "event_starts_at_1i"
+  context "when done" do
+    before do
+      fill_in "Name", with: "Code and Coffee"
+      fill_in "Description", with: "Sling some code with a cup o' Joe!"
+      fill_in "Location", with: "Portland, OR"
+      fill_in "Price", with: 0.00
+      #select (10.days.from_now).to_s, from: "event_starts_at_1i"
 
-    click_button "Create Event"
+      click_button "Create Event"
+    end
 
-    expect(page).to have_text "Code and Coffee"
+    it "redirects to the detail page" do
+      expect(current_path).to eq event_path(Event.last)
+    end
+
+    it "shows the new event's details" do
+      expect(page).to have_text "Code and Coffee"
+    end
   end
 end
--- spec/features/navigate_events_spec.rb
+++ spec/features/navigate_events_spec.rb
@@ -4,28 +4,6 @@ require "support/attributes"
 describe "Navigating events" do
   let!(:event) { Event.create event_attributes }
 
-  it "allows navigation from the listing page to the create page" do
-    visit events_url
-
-    click_link "Add New Event"
-
-    expect(current_path).to eq new_event_path
-  end
-
-  it "allows navigation from the create page to the detail page" do
-    visit events_url
-    click_link "Add New Event"
-    fill_in "Name", with: "Code and Coffee"
-    fill_in "Description", with: "Sling some code with a cup o' Joe!"
-    fill_in "Location", with: "Portland, OR"
-    fill_in "Price", with: 0.00
-    #select (10.days.from_now).to_s, from: "event_starts_at_1i"
-
-    click_button "Create Event"
-
-    expect(current_path).to eq event_path(Event.last)
-  end
-
   it "allows navigation from the detail page to the listing page destroying an event" do
     visit event_url(event)
 
