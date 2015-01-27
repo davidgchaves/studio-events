cd7ee32b4c848851b0ffbc38f41336674e1a29c0
David G Chaves <davidgchaves@gmail.com>
2015-01-27 09:18:26 +0100

    Clean up "Editing an event" scenario

--- spec/features/edit_event_spec.rb
+++ spec/features/edit_event_spec.rb
@@ -2,23 +2,33 @@ require "rails_helper"
 require "support/attributes"
 
 describe "Editing an event" do
-  it "shows the form to edit events" do
-    event = Event.create event_attributes
+  let!(:event) { Event.create event_attributes }
+
+  before do
     visit event_url(event)
-
     click_link "Edit"
+  end
 
+  it "goes to the edit page" do
+    expect(current_path).to eq edit_event_path(event)
+  end
+
+  it "shows the form to edit events" do
     expect(find_field('Name').value).to eq event.name
   end
 
-  it "shows the event's updated details" do
-    event = Event.create event_attributes
-    visit event_url(event)
-    click_link "Edit"
-    fill_in "Name", with: "Updated Event Name"
+  context "when done" do
+    before do
+      fill_in "Name", with: "Updated Event Name"
+      click_button "Update Event"
+    end
 
-    click_button "Update Event"
+    it "redirects to the detail page" do
+      expect(current_path).to eq event_path(event)
+    end
 
-    expect(page).to have_text "Updated Event Name"
+    it "shows the event's updated details" do
+      expect(page).to have_text "Updated Event Name"
+    end
   end
 end
--- spec/features/navigate_events_spec.rb
+++ spec/features/navigate_events_spec.rb
@@ -4,23 +4,6 @@ require "support/attributes"
 describe "Navigating events" do
   let!(:event) { Event.create event_attributes }
 
-  it "allows navigation from the detail page to the edit page" do
-    visit event_url(event)
-
-    click_link "Edit"
-
-    expect(current_path).to eq edit_event_path(event)
-  end
-
-  it "allows navigation from the edit page to the detail page" do
-    visit event_url(event)
-    click_link "Edit"
-
-    click_button "Update Event"
-
-    expect(current_path).to eq event_path(event)
-  end
-
   it "allows navigation from the listing page to the create page" do
     visit events_url
 
