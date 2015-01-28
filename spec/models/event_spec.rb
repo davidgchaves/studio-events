59ad27ba948af4d433098596046e70f7b872b914
David G Chaves <davidgchaves@gmail.com>
2015-01-28 13:19:00 +0100

    GREEN: Viewing the list of events does not show a past event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -18,4 +18,24 @@ describe "An event" do
 
     expect(event.free?).to eq false
   end
+
+  it "is upcoming if the starts at date is in the future" do
+    event = Event.create starts_at: 3.days.from_now
+
+    expect(Event.upcoming).to include event
+  end
+
+  it "is not upcoming if the starts at date is in the past" do
+    event = Event.create starts_at: 3.days.ago
+
+    expect(Event.upcoming).not_to include event
+  end
+
+  it "returns upcoming events ordered with the most recently-upcoming event first" do
+    event1 = Event.create starts_at: 3.months.from_now
+    event2 = Event.create starts_at: 2.months.from_now
+    event3 = Event.create starts_at: 1.months.from_now
+
+    expect(Event.upcoming).to eq [event3, event2, event1]
+  end
 end
