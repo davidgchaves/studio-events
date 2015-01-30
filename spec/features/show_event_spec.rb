02e5d9fd6909e36d26145a24c667848fd527613e
David G Chaves <davidgchaves@gmail.com>
2015-01-30 13:16:51 +0100

    RED: Viewing an event always shows an image (event's or default)

--- spec/features/show_event_spec.rb
+++ spec/features/show_event_spec.rb
@@ -12,7 +12,7 @@ describe "Viewing an individual event" do
     expect(page).to have_text event.description
     expect(page).to have_text event.starts_at
     expect(page).to have_text event.capacity
-    expect(page).to have_selector "img[src$='#{event.image_file_name}']"
+    # expect(page).to have_selector "img[src$='#{event.image_file_name}']"
   end
 
   it "shows the price if the price is not $0" do
@@ -29,6 +29,22 @@ describe "Viewing an individual event" do
     visit event_url(event)
 
     expect(page).to have_text "Free"
+  end
+
+  it "shows the event image when there's an image associated to the event" do
+    event_with_image = Event.create event_attributes(image_file_name: "coffeecode.png")
+
+    visit event_url(event_with_image)
+
+    expect(page).to have_selector "img[src$='#{event_with_image.image_file_name}']"
+  end
+
+  it "shows a default image when there's no image associated to the event" do
+    event_with_no_image = Event.create event_attributes(image_file_name: "")
+
+    visit event_url(event_with_no_image)
+
+    expect(page).to have_selector "img[src$='placeholder.png']"
   end
 
   it "allows navigation to the listing page" do
