f4fdc92def6ba8a81d0a10d42d877ea593f0fc37
David G Chaves <davidgchaves@gmail.com>
2015-01-30 13:18:44 +0100

    GREEN: Viewing an event always shows an image (event's or default)

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
   def format_price(event)
     event.free? ? content_tag(:strong, "Free") : number_to_currency(event.price)
   end
+
+  def image_for(event)
+    event.image_file_name.blank? ? image_tag("placeholder.png") : image_tag(event.image_file_name)
+  end
 end
