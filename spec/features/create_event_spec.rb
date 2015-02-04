707a86547fb564841bcea2e846771f7d1f995909
David G Chaves <davidgchaves@gmail.com>
2015-02-04 20:47:31 +0100

    RED: Creating a new event on failure shows what was wrong last time

--- spec/features/create_event_spec.rb
+++ spec/features/create_event_spec.rb
@@ -18,7 +18,7 @@ describe "Creating a new event" do
     expect(find_field("Image Filename").value).to eq nil
   end
 
-  context "when done" do
+  context "on success" do
     before do
       fill_in "Name", with: "Code and Coffee"
       fill_in "Description", with: "Sling some code with a cup o' Joe!"
@@ -37,6 +37,27 @@ describe "Creating a new event" do
 
     it "shows the new event's details" do
       expect(page).to have_text "Code and Coffee"
+    end
+  end
+
+  context "on failure" do
+    before do
+      fill_in "Name", with: ""
+      fill_in "Description", with: "Sling some code with a cup o' Joe!"
+
+      click_button "Create Event"
+    end
+
+    it "renders again the create page" do
+      expect(current_path).to eq events_path
+    end
+
+    it "shows what was wrong last time" do
+      expect(page).to have_text "correct the following"
+    end
+
+    it "preserves the event info entered the last time" do
+      expect(page).to have_text "Sling some code with a cup o' Joe"
     end
   end
 end
