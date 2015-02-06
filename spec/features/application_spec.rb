00396da0e7570b61cb0b5c302f206b362dbc2711
David G Chaves <davidgchaves@gmail.com>
2015-02-06 12:54:31 +0100

    RED: The flash is not shown with an empty alert message

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -6,4 +6,10 @@ describe "The flash" do
 
     expect(page).not_to have_css "p.flash.notice"
   end
+
+  it "is not shown with an empty alert message" do
+    visit events_url
+
+    expect(page).not_to have_css "p.flash.alert"
+  end
 end
