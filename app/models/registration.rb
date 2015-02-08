f9fc30d62daf5319aec4850b8847f5797f5bdf4e
David G Chaves <davidgchaves@gmail.com>
2015-02-08 20:35:12 +0100

    GREEN: A registration requires a well formatted email

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,4 +2,6 @@ class Registration < ActiveRecord::Base
   belongs_to :event
 
   validates :name, presence: true
+  validates :email, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
