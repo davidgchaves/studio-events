84dc2f8ffd5f0d8310402c31df71dfe4bf4d55f2
David G Chaves <davidgchaves@gmail.com>
2015-02-08 20:27:05 +0100

    RED: A registration requires a well formatted email

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -17,5 +17,29 @@ describe "A registration" do
 
     expect(invalid_registration.errors[:name].any?).to eq true
   end
+
+  it "accepts properly formatted emails" do
+    valid_emails = ["moe.sTOO@stoogies.com", "moe.sTOO@st.o.gies.com"]
+
+    valid_emails.each do |valid_email|
+      valid_registration = Registration.new email: valid_email
+
+      valid_registration.valid?
+
+      expect(valid_registration.errors[:email].any?).to eq false
+    end
+  end
+
+  it "rejects improperly formatted emails" do
+    invalid_emails = ["@", "moe stoo@stoogies.com", "moe.stoo@sto ogies.com"]
+
+    invalid_emails.each do |invalid_email|
+      invalid_registration = Registration.new email: invalid_email
+
+      invalid_registration.valid?
+
+      expect(invalid_registration.errors[:email].any?).to eq true
+    end
+  end
 end
 
