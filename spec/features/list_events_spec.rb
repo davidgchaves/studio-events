51f6622340bbe67c554310ebb426e66af00aec4d
David G Chaves <davidgchaves@gmail.com>
2015-02-10 19:50:55 +0100

    Refactor list events spec

--- spec/features/list_events_spec.rb
+++ spec/features/list_events_spec.rb
@@ -1,41 +1,14 @@
 require "rails_helper"
 
 describe "Viewing the list of events" do
-  let!(:event1) { Event.create name: "BugSmash",
-                               location: "Denver",
-                               price: 10.00,
-                               description: "A fun evening of bug smashing",
-                               starts_at: 10.days.from_now,
-                               capacity: 55,
-                               image_file_name: "bugsmash.png" }
-
-  let!(:past_event) { Event.create name: "Coffee and Code",
-                                   location: "Stumpron Coffee, Portland",
-                                   price: 0.00,
-                                   description: "Start your day off right with a delicious cup of coffee",
-                                   starts_at: 1.year.ago,
-                                   capacity: 111,
-                                   image_file_name: "coffeecode.png" }
-
-  let!(:event2) { Event.create name: "Hackathon",
-                               location: "Austin",
-                               price: 15.00,
-                               description: "Hunker down at the Hackathon",
-                               starts_at: 15.days.from_now,
-                               capacity: 222,
-                               image_file_name: "hackathon.png" }
-
-  let!(:event3) { Event.create name: "Kata Camp",
-                               location: "Dallas",
-                               price: 75.00,
-                               description: "Practice your craft kata style",
-                               starts_at: 30.days.from_now,
-                               capacity: 333,
-                               image_file_name: "katacamp.png" }
-
-  before { visit events_url }
+  let!(:event1) { Event.create event_attributes(name: "BugSmash") }
+  let!(:event2) { Event.create event_attributes(name: "Hackathon") }
+  let!(:event3) { Event.create event_attributes(name: "Kata Camp") }
+  let!(:past_event) { Event.create event_attributes(name: "Coffee and Code", starts_at: 1.year.ago) }
 
   it "shows the upcoming events" do
+    visit events_url
+
     expect(page).to have_text "Events"
     expect(page).to have_text event1.name
     expect(page).to have_text event2.name
@@ -50,10 +23,14 @@ describe "Viewing the list of events" do
   end
 
   it "does not show a past event" do
+    visit events_url
+
     expect(page).not_to have_text past_event.name
   end
 
   it "allows navigation to the event's detail page" do
+    visit events_url
+
     click_link event2.name
 
     expect(current_path).to eq event_path(event2)
