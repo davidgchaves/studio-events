f07b613960cb7eab718a4232e2c7ee61cf1823d3
David G Chaves <davidgchaves@gmail.com>
2015-02-10 09:38:50 +0100

    Refactor create registration spec

--- spec/features/create_registration_spec.rb
+++ spec/features/create_registration_spec.rb
@@ -2,21 +2,18 @@ require 'rails_helper'
 require 'support/attributes'
 
 describe "Creating a new registration for an event" do
-  it "goes to the create event's registration page" do
-    event = Event.create event_attributes
+  let(:event) { Event.create event_attributes }
+
+  before do
     visit event_url(event)
-
     click_link "Register!"
+  end
 
+  it "goes to the create event's registration page" do
     expect(current_path).to eq new_event_registration_path(event)
   end
 
   it "shows a header with the event for which we are registering" do
-    event = Event.create event_attributes
-    visit event_url(event)
-
-    click_link "Register!"
-
     expect(page).to have_text event.name
   end
 end
