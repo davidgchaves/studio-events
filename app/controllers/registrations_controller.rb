d55cc0658bc8526b3f5913c88a7b7a42b5e8dd91
David G Chaves <davidgchaves@gmail.com>
2015-02-10 11:14:32 +0100

    DRY registrations controller with the before_action filter

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,15 @@
 class RegistrationsController < ApplicationController
+  before_action :set_event, only: [:index, :new, :create]
+
   def index
-    @event = Event.find params[:event_id]
     @registrations = @event.registrations
   end
 
   def new
-    @event = Event.find params[:event_id]
     @registration = @event.registrations.new
   end
 
   def create
-    @event = Event.find params[:event_id]
     @registration = @event.registrations.new registration_params
     if @registration.save
       redirect_to event_registrations_path(@event), notice: "Thanks, you're registered!"
@@ -22,5 +21,9 @@ class RegistrationsController < ApplicationController
   private
     def registration_params
       params.require(:registration).permit :name, :email, :how_heard
+    end
+
+    def set_event
+      @event = Event.find params[:event_id]
     end
 end
