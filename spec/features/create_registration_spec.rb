4926629160e86cb96577fd34fca01c75ae4f41a1
David G Chaves <davidgchaves@gmail.com>
2015-02-10 09:33:13 +0100

    RED: Creating a new registration for an event...
    ...shows a header with the event for which we are registering

--- spec/features/create_registration_spec.rb
+++ spec/features/create_registration_spec.rb
@@ -10,4 +10,13 @@ describe "Creating a new registration for an event" do
 
     expect(current_path).to eq new_event_registration_path(event)
   end
+
+  it "shows a header with the event for which we are registering" do
+    event = Event.create event_attributes
+    visit event_url(event)
+
+    click_link "Register!"
+
+    expect(page).to have_text event.name
+  end
 end
