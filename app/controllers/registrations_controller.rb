32fb7488b82816140da919cabd1bf230923105db
David G Chaves <davidgchaves@gmail.com>
2015-02-10 10:22:29 +0100

    GREEN: ...redirects to the registration's listing page for that event

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,9 @@ class RegistrationsController < ApplicationController
     @event = Event.find params[:event_id]
     @registration = @event.registrations.new
   end
+
+  def create
+    @event = Event.find params[:event_id]
+    redirect_to event_registrations_path(@event)
+  end
 end
