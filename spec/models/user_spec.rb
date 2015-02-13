0f4f70535b750b8c1c85b1352af48acac32664a9
David G Chaves <davidgchaves@gmail.com>
2015-02-13 11:56:24 +0100

    RED: A user email...
    ...accepts(/rejects) properly(/improperly) formatted emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,28 @@ describe User do
       expect(user.errors[:email].any?).to be_truthy
     end
   end
+
+  context "email" do
+    it "accepts properly formatted emails" do
+      valid_emails = ["moe.sTOO@stoogies.com", "moe.sTOO@st.o.gies.com"]
+      valid_emails.each do |valid_email|
+        valid_user = User.new email: valid_email
+
+        valid_user.valid?
+
+        expect(valid_user.errors[:email].any?).to be_falsy
+      end
+    end
+
+    it "rejects improperly formatted emails" do
+      invalid_emails = ["@", "moe stoo@stoogies.com", "moe.stoo@sto ogies.com"]
+      invalid_emails.each do |invalid_email|
+        invalid_user = User.new email: invalid_email
+
+        invalid_user.valid?
+
+        expect(invalid_user.errors[:email].any?).to be_truthy
+      end
+    end
+  end
 end
