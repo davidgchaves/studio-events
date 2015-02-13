4a2e116393c43c18110541a15b4eb0ff7ff65e47
David G Chaves <davidgchaves@gmail.com>
2015-02-13 11:58:38 +0100

    GREEN: ...accepts(/rejects) properly(/improperly) formatted emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :name
+  validates :email, presence: true,
+                    format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 end
