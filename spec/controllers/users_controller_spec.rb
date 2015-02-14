bc0089b1a3ea75ba648d204d301b346b3f84139f
David G Chaves <davidgchaves@gmail.com>
2015-02-14 12:52:40 +0100

    Refactor users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 describe UsersController do
 
   describe "GET #index" do
+    let(:larry) { FactoryGirl.create :user, name: "Larry" }
+    let(:moe) { FactoryGirl.create :user, name: "Moe" }
+    before(:each) { get :index }
+
     it "populates an array of all users" do
-      larry = FactoryGirl.create :user, name: "Larry"
-      moe = FactoryGirl.create :user, name: "Moe"
-
-      get :index
-
       expect(assigns :users).to match_array [larry, moe]
     end
 
     it "renders the :index template" do
-      get :index
-
       expect(response).to render_template :index
     end
   end
