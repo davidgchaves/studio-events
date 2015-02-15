0b6f835fb6f7a2f0290840bb5018311d4c789c87
David G Chaves <davidgchaves@gmail.com>
2015-02-15 18:42:21 +0100

    Add Sign Up link and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-
   root "events#index"
+
   resources :events do
     resources :registrations
   end
+
+  get "signup" => "users#new"
+  resources :users
 end
