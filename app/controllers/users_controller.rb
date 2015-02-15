10c9aafa634cd5f0820a73055ff0d926ce73fb36
David G Chaves <davidgchaves@gmail.com>
2015-02-15 20:12:49 +0100

    GREEN: ...redirects to users#show
    The users controller POST #create (with a valid user)...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     @user.save
-    render :new
+    redirect_to @user
   end
 
   private
-
     def user_params
       params.require(:user).permit :name, :email, :password, :password_confirmation
     end
