2cecf8348f83517012cc0e3cb07a5e0aca0dafec
David G Chaves <davidgchaves@gmail.com>
2015-02-19 09:52:32 +0100

    Add an event association to the registration factory

--- spec/factories/registrations.rb
+++ spec/factories/registrations.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :registration do
+    association :event
     sequence(:name) { |n| "Moe #{n}" }
     sequence(:email) { |n| "moe#{n}@stoogies.com" }
     how_heard "Web Search"
