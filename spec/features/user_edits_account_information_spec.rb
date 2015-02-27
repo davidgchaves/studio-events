bc3932e8c1ee09a5d0bac402ca803345e369e4dd
David G Chaves <davidgchaves@gmail.com>
2015-02-27 13:47:40 +0100

    Handle user introducing wrong data when updating his account

--- spec/features/user_edits_account_information_spec.rb
+++ spec/features/user_edits_account_information_spec.rb
@@ -14,4 +14,17 @@ feature "User edits account information" do
     expect(page).to have_css "#users h2", text: "MAD_LARRY@example.com"
     expect(page).not_to have_css "#users h2", text: "calm_larry@example.com"
   end
+
+  scenario "unsuccessfully" do
+    larry = FactoryGirl.create :user
+    visit user_path(larry)
+
+    click_on "Edit Account"
+
+    fill_in "Email", with: ""
+    click_on "Update Account"
+
+    expect(page).to have_css "#errors h2", text: "The user could not be saved"
+    expect(page).to have_css "li.required .field_with_errors label", text: "Email"
+  end
 end
